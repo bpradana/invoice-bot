8fcc5f599c14a6016a48fa8db8e2818ec1948f02
Bintang Pradana Erlangga Putra <bintangpradana02@gmail.com>
2022-09-05 08:21:35 +0700

    remove token

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,5 +2,5 @@ import { AdapterOptions } from '@enitoni/gears-discordjs';
 
 export const adapterOptions: AdapterOptions = {
   token:
-    'OTU1MDUyMTA4MDQwNzczNjUy.GCEMLC.3-btLAnldh6GzbEBAQJ9gd924xFVG99HzQHVPM',
+    'your_bot_token',
 };
